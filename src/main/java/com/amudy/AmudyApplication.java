7c0e21b5da1546b84bed1e671e66ba3f0d4a40b5
Shin <dbrms1a1a@naver.com>
2024-04-19 15:48:18 +0900

    intial commit

---
+++ src/main/java/com/amudy/AmudyApplication.java
@@ -0,0 +1,13 @@
+package com.amudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amudy/AmudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
